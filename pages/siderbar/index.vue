89eb8029381017278dab683294260ed3d0e604f7
luckboyi <546217953@qq.com>
2019-04-08 14:38:10 +0800

    修改

---
+++ pages/siderbar/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sidebarCon"></div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.sidebarCon{
+  width: 368px;
+  position: fixed;
+  height: 100%;
+  background: red;
+}
+</style>
+
